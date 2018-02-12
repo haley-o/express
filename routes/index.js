b5f72002c10385f8a1e807e20b7eb260d6873110
Haley <h_ostrander@outlook.com>
2018-02-12 12:33:24 -0500

    adding in broken code that doesn't work

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+ { name: "Jade", usefulness: "sucky" },
+ { name: "EJS", usefulness: "a little better" },
+ { name: "handlebars", usefulness: "awesome" }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: "I'm having so much fun" }); //getting our index and running data into it, we are making stuff up and adding it through????
+ res.render('index', {
+ title: 'Express',
+ message: "I need vacations",
+ templates: templateTypes
+ });
 });
 
 module.exports = router;
