b7085feded3e6f05a76f2bc94b1f0309878ce76f
Haley <h_ostrander@outlook.com>
2018-02-12 13:40:05 -0500

    code is still broken

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+
+host : "localhost",
+port : 8888,
+user : "root",
+password : "",
+database : "cooperstuff"
+
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mysql = require('mysql');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 const templateTypes = [
@@ -7,13 +9,25 @@ const templateTypes = [
  { name: "handlebars", usefulness: "awesome" }
 ]
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.render('index', {
- title: 'Express',
- message: "I need vacations",
- templates: templateTypes
- });
+
+	connect.query('SELECT * FROM mainmodel', (err, result, fields) => {
+		if (err) {
+			throw err; console.log(err);
+		}else {
+			console.log(result);
+		}
+	});
+
+	 res.render('home', {
+	 title: 'Express',
+	 message: "I need vacations",
+	 templates: templateTypes,
+	 anothermessage : "handlebars is awesome"
+	 });
 });
 
 module.exports = router;
